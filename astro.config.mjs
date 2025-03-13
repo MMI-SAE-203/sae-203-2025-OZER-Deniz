2277824da1c195b7f042316b7c06c033c942ccd7
OZER-Deniz <deniz.ozer@edu.univ-fcomte.fr>
2025-03-13 10:21:42 +0100

    dynamique film

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
     integrations: [tailwind()],
     experimental: { svg: true },
+    output: 'server'
 });
--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,49 +1,62 @@
 import PocketBase from 'pocketbase';
 const pb = new PocketBase('http://127.0.0.1:8090');
+export default pb;
+
+export async function allFilms() {
+    return await pb.collection('Film').getFullList({
+        fields: 'id,titre_film,affiche_film,diffusion_film'
+    });
+}
+
+export async function oneFilm(id) {
+    return await pb.collection('Film').getOne(id, {
+        fields: 'titre_film,duree_film,langue_film,production_film,annee_de_sortie_film,genre_film,affiche_film'
+    });
+}
 
 export async function allFilmsSorted() {
-    return await pb.collection('films').getFullList({
+    return await pb.collection('Film').getFullList({
         sort: 'dateProjection'
     });
 }
 
 export async function allActivitiesSorted() {
-    return await pb.collection('activites').getFullList({
+    return await pb.collection('Activite').getFullList({
         sort: 'dateProjection'
     });
 }
 
 export async function allParticipantsSorted() {
-    return await pb.collection('participants').getFullList({
+    return await pb.collection('Invite').getFullList({
         sort: 'nom'
     });
 }
 
 export async function oneFilm(id) {
-    return await pb.collection('films').getOne(id);
+    return await pb.collection('Film').getOne(id);
 }
 
 export async function oneActivity(id) {
-    return await pb.collection('activites').getOne(id);
+    return await pb.collection('Activite').getOne(id);
 }
 
 export async function oneParticipant(id) {
-    return await pb.collection('participants').getOne(id);
+    return await pb.collection('Invite').getOne(id);
 }
 
 export async function allActivitiesByAnimatorId(animatorId) {
-    return await pb.collection('activites').getFullList({
+    return await pb.collection('Activite').getFullList({
         filter: `animateur = '${animatorId}'`
     });
 }
 
 export async function allActivitiesByAnimatorName(animatorName) {
-    const animators = await pb.collection('animateurs').getFullList({
+    const animators = await pb.collection('Invite').getFullList({
         filter: `nom = '${animatorName}'`
     });
     if (animators.length === 0) return [];
-    return await pb.collection('activites').getFullList({
-        filter: `animateur = '${animators[0].id}'`
+    return await pb.collection('Activite').getFullList({
+        filter: `Invite = '${animators[0].id}'`
     });
 }
 
